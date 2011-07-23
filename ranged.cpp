78e63b7268df8699e34ea6ebbbe0a5319d56e3c3
Head <sebbebroberg@hotmail.com>
2011-07-23 02:30:10 +0200

    There we go. Latest pull compiles and seem to work. Good night
    Signed-off-by: Head <sebbebroberg@hotmail.com>

--- ranged.cpp
+++ ranged.cpp
@@ -6,117 +6,8 @@
 #include "line.h"
 #include "skill.h"
 #include "rng.h"
-#if (defined _WIN32 || defined __WIN32__) && ! defined __CYGWIN__
-/* Windows platforms.  */
-
-#   ifdef __cplusplus
-extern "C" {
-#   endif
-
-struct _timespec
-{
-  time_t tv_sec;
-  long int tv_nsec;
-};
-enum { BILLION = 1000 * 1000 * 1000 };
-#   ifdef __cplusplus
-}
-#   endif
 
 
-
-# define WIN32_LEAN_AND_MEAN
-# include <windows.h>
-
-/* The Win32 function Sleep() has a resolution of about 15 ms and takes
-   at least 5 ms to execute.  We use this function for longer time periods.
-   Additionally, we use busy-looping over short time periods, to get a
-   resolution of about 0.01 ms.  In order to measure such short timespans,
-   we use the QueryPerformanceCounter() function.  */
-
-int
-_nanosleep (const struct _timespec *requested_delay,
-           struct _timespec *remaining_delay)
-{
-  static bool initialized;
-  /* Number of performance counter increments per nanosecond,
-     or zero if it could not be determined.  */
-  static double ticks_per_nanosecond;
-
-  if (requested_delay->tv_nsec < 0 || BILLION <= requested_delay->tv_nsec)
-    {
-      errno = EINVAL;
-      return -1;
-    }
-
-  /* For requested delays of one second or more, 15ms resolution is
-     sufficient.  */
-  if (requested_delay->tv_sec == 0)
-    {
-      if (!initialized)
-        {
-          /* Initialize ticks_per_nanosecond.  */
-          LARGE_INTEGER ticks_per_second;
-
-          if (QueryPerformanceFrequency (&ticks_per_second))
-            ticks_per_nanosecond =
-              (double) ticks_per_second.QuadPart / 1000000000.0;
-
-          initialized = true;
-        }
-      if (ticks_per_nanosecond)
-        {
-          /* QueryPerformanceFrequency worked.  We can use
-             QueryPerformanceCounter.  Use a combination of Sleep and
-             busy-looping.  */
-          /* Number of milliseconds to pass to the Sleep function.
-             Since Sleep can take up to 8 ms less or 8 ms more than requested
-             (or maybe more if the system is loaded), we subtract 10 ms.  */
-          int sleep_millis = (int) requested_delay->tv_nsec / 1000000 - 10;
-          /* Determine how many ticks to delay.  */
-          LONGLONG wait_ticks = requested_delay->tv_nsec * ticks_per_nanosecond;
-          /* Start.  */
-          LARGE_INTEGER counter_before;
-          if (QueryPerformanceCounter (&counter_before))
-            {
-              /* Wait until the performance counter has reached this value.
-                 We don't need to worry about overflow, because the performance
-                 counter is reset at reboot, and with a frequency of 3.6E6
-                 ticks per second 63 bits suffice for over 80000 years.  */
-              LONGLONG wait_until = counter_before.QuadPart + wait_ticks;
-              /* Use Sleep for the longest part.  */
-              if (sleep_millis > 0)
-                Sleep (sleep_millis);
-              /* Busy-loop for the rest.  */
-              for (;;)
-                {
-                  LARGE_INTEGER counter_after;
-                  if (!QueryPerformanceCounter (&counter_after))
-                    /* QueryPerformanceCounter failed, but succeeded earlier.
-                       Should not happen.  */
-                    break;
-                  if (counter_after.QuadPart >= wait_until)
-                    /* The requested time has elapsed.  */
-                    break;
-                }
-              goto done;
-            }
-        }
-    }
-  /* Implementation for long delays and as fallback.  */
-  Sleep (requested_delay->tv_sec * 1000 + requested_delay->tv_nsec / 1000000);
-
- done:
-  /* Sleep is not interruptible.  So there is no remaining delay.  */
-  if (remaining_delay != NULL)
-    {
-      remaining_delay->tv_sec = 0;
-      remaining_delay->tv_nsec = 0;
-    }
-  return 0;
-}
-
-#endif
 
 void game::fire(player &p, int tarx, int tary, std::vector<point> &trajectory,
                 bool burst)
@@ -214,11 +105,8 @@ void game::fire(player &p, int tarx, int tary, std::vector<point> &trajectory,
  else if (!is_bolt)
   sound(p.posx, p.posy, noise, gunsound);
 // Set up a timespec for use in the nanosleep function below
-#if (defined _WIN32 || defined __WIN32__) && ! defined __CYGWIN__
-  _timespec ts;
-#else
     timespec ts;
-#endif
+
    ts.tv_sec = 0;
   ts.tv_nsec = BULLET_SPEED;
 
@@ -318,11 +206,7 @@ missed_by, deviation, trange, p.weapon.charges, p.posx, p.posy, tarx, tary);
     mvwputch(w_terrain, trajectory[i].y + SEEY - u.posy,
                         trajectory[i].x + SEEX - u.posx, c_red, bullet);
     wrefresh(w_terrain);
-    #if (defined _WIN32 || defined __WIN32__) && ! defined __CYGWIN__
-    _nanosleep(&ts, NULL);
-    #else
     nanosleep(&ts,NULL);
-    #endif
    }
 
    if (dam <= 0) {
@@ -787,7 +671,7 @@ void game::hit_monster_with_flags(monster &z, unsigned int flags)
    z.add_effect(ME_ONFIRE, rng(8, 20));
   else if (z.made_of(FLESH))
    z.add_effect(ME_ONFIRE, rng(5, 10));
-  
+
  } else if (flags & mfb(WF_AMMO_INCENDIARY)) {
 
   if (z.made_of(VEGGY) || z.made_of(COTTON) || z.made_of(WOOL) ||
